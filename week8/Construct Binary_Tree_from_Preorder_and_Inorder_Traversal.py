82edbe5723428b8a5eb2ba7c0d679c58496f6def
wontak <ryu071511@gmail.com>
2019-12-11 19:15:33 +0900

    add

--- week8/Construct Binary_Tree_from_Preorder_and_Inorder_Traversal.py
+++ week8/Construct Binary_Tree_from_Preorder_and_Inorder_Traversal.py
@@ -30,16 +30,14 @@ class Solution:
         return d[target] - d[candi]
 
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        '''O(N)/O(N)'''
         if not preorder: return None
         inorder_dict = {}
         for i in range(len(inorder)):
             inorder_dict[inorder[i]] = i
         root = TreeNode(preorder[0]) 
-        curr, stack = root, [root]
-        i = 1
+        curr, stack, i = root, [root], 1
         while i < len(preorder):
-            if curr.left is None and inorder_dict[curr.val] - inorder_dict[preorder[i]] > 0: # inorder 순서
+            if curr.left is None and self.get_distance(preorder[i], curr.val, inorder_dict) < 0:
                 curr.left = TreeNode(preorder[i])
                 curr, stack, i = curr.left, stack + [curr.left], i + 1
             elif curr.right is None and self.get_distance(preorder[i], curr.val, inorder_dict) > 0:
@@ -48,9 +46,19 @@ class Solution:
                 while stack and before >= after and after > 0:
                     curr = stack.pop()
                     before = self.get_distance(preorder[i], curr.val, inorder_dict)
-                    if stack:
-                        after = self.get_distance(preorder[i], stack[-1].val, inorder_dict)
+                    if stack: after = self.get_distance(preorder[i], stack[-1].val, inorder_dict)
                     else: break
                 curr.right = TreeNode(preorder[i])
                 curr, stack, i = curr.right, stack + [curr.right], i + 1
         return root
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        def build(stop):
+            if inorder and inorder[0] != stop:
+                root = TreeNode(preorder.pop(0))
+                root.left = build(root.val)
+                inorder.pop(0)
+                root.right = build(stop)
+                return root
+        return build(None)
