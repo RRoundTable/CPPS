a5dadc8605b86fae6cf892776819a3c6fa22f971
wontak <ryu071511@gmail.com>
2020-02-11 15:25:03 +0900

    add

---
+++ DP/Cherry_Pickup.py
@@ -0,0 +1,82 @@
+'''
+link: https://leetcode.com/problems/cherry-pickup/
+In a N x N grid representing a field of cherries, each cell is one of three possible integers.
+
+ 
+
+0 means the cell is empty, so you can pass through;
+1 means the cell contains a cherry, that you can pick up and pass through;
+-1 means the cell contains a thorn that blocks your way.
+ 
+
+Your task is to collect maximum number of cherries possible by following the rules below:
+
+ 
+
+Starting at the position (0, 0) and reaching (N-1, N-1) by moving right or down through valid path cells (cells with value 0 or 1);
+After reaching (N-1, N-1), returning to (0, 0) by moving left or up through valid path cells;
+When passing through a path cell containing a cherry, you pick it up and the cell becomes an empty cell (0);
+If there is no valid path between (0, 0) and (N-1, N-1), then no cherries can be collected.
+ 
+
+ 
+
+Example 1:
+
+Input: grid =
+[[0, 1, -1],
+ [1, 0, -1],
+ [1, 1,  1]]
+Output: 5
+Explanation: 
+The player started at (0, 0) and went down, down, right right to reach (2, 2).
+4 cherries were picked up during this single trip, and the matrix becomes [[0,1,-1],[0,0,-1],[0,0,0]].
+Then, the player went left, up, up, left to return home, picking up one more cherry.
+The total number of cherries picked up is 5, and this is the maximum possible.
+ 
+
+Note:
+
+grid is an N by N 2D array, with 1 <= N <= 50.
+Each grid[i][j] is an integer in the set {-1, 0, 1}.
+It is guaranteed that grid[0][0] and grid[N-1][N-1] are not -1.
+'''
+
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+        '''Top-Down'''
+        N = len(grid)
+        dp = [[[None] * N  for _ in range(N)] for _ in range(N)]
+        def recursion(r1, c1, c2):
+            r2 = r1 + c1 - c2
+            if N in {r1, r2, c1, c2} or -1 in {grid[r1][c1], grid[r2][c2]}:
+                return float('-inf')
+            elif r1 == c1 == N - 1:
+                return grid[r1][c1]
+            elif dp[r1][c1][c2]: return dp[r1][c1][c2]
+            else:
+                res = grid[r1][c1] + (c1 != c2) * grid[r2][c2]
+                res += max(recursion(r1+1, c1, c2), 
+                           recursion(r1, c1+1, c2), 
+                           recursion(r1, c1+1, c2+1), 
+                           recursion(r1+1, c1, c2+1))
+                dp[r1][c1][c2] = res
+            return res
+        return max(0, recursion(0, 0, 0))
+    
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+        '''Bottom-up'''
+        N = len(grid)
+        dp = [[float('-inf')] * N for _ in range(N)]
+        dp[0][0] = grid[0][0]
+        for t in range(1, 2*N - 1):
+            dp2 = [[float('-inf')] * N for _ in range(N)]
+            for i in range(max(0, t - (N-1)), min(N-1, t) + 1):
+                for j in range(max(0, t - (N-1)), min(N-1, t) + 1):
+                    print(t, i, j)
+                    if -1 in {grid[i][t-i], grid[j][t-j]}: continue
+                    val = grid[i][t-i] + (i != j) * grid[j][t-j]
+                    dp2[i][j] = max(dp[pi][pj] for pi in (i-1, i) for pj in (j-1, j) if pi >= 0 and pj >= 0) + val
+            dp = dp2
+        return max(0, dp[-1][-1])
