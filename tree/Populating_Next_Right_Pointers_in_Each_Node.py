e7c11b6d2a7c75ddad75b7f994a8c0e1f30e2ce9
wontak <ryu071511@gmail.com>
2020-02-21 12:22:38 +0900

    add

---
+++ tree/Populating_Next_Right_Pointers_in_Each_Node.py
@@ -0,0 +1,49 @@
+'''
+link: https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+ 
+
+Follow up:
+
+You may only use constant extra space.
+Recursive approach is fine, you may assume implicit stack space does not count as extra space for this problem.
+
+
+Input: root = [1,2,3,4,5,6,7]
+Output: [1,#,2,3,#,4,5,6,7,#]
+Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+'''
+
+from collections import deque
+
+
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        ans, queue = root, deque([root])
+        while root and queue:
+            for i in range(len(queue)):
+                node = queue.popleft()
+                node.next = queue[0] if queue else None
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+            node.next = None
+        return ans
