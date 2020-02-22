fa6ac98b9efc34fafe4490b1ae39d6c097953360
wontak <ryu071511@gmail.com>
2020-02-22 22:43:32 +0900

    add

---
+++ tree/Lowest_Common_Ancestor_of_a_Binary_Tree.py
@@ -0,0 +1,48 @@
+'''
+link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
+
+
+ 
+
+Example 1:
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+Example 2:
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+ 
+
+Note:
+
+All of the nodes' values will be unique.
+p and q are different and both values will exist in the binary tree.
+'''
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        lca = None
+        def have(node):
+            nonlocal lca
+            if not lca and node:
+                curr, left, right = node in [p, q], have(node.left), have(node.right)
+                if not lca and curr +left + right == 2: lca = node
+                return curr + left + right
+            return 0
+        have(root)
+        return lca
