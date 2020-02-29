33078690f2df23391ecee467339897e234da8091
wontak <ryu071511@gmail.com>
2020-02-29 13:34:46 +0900

    graph

---
+++ graph/Course_Schedule_II.py
@@ -0,0 +1,52 @@
+'''
+link: https://leetcode.com/problems/course-schedule-ii/
+There are a total of n courses you have to take, labeled from 0 to n-1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
+
+There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
+
+Example 1:
+
+Input: 2, [[1,0]] 
+Output: [0,1]
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished   
+             course 0. So the correct course order is [0,1] .
+Example 2:
+
+Input: 4, [[1,0],[2,0],[3,1],[3,2]]
+Output: [0,1,2,3] or [0,2,1,3]
+Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both     
+             courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0. 
+             So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3] .
+Note:
+
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
+
+'''
+from collections import defaultdict
+
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        ans, edges, color, is_possible = [None] * numCourses, defaultdict(list), {n: 0 for n in range(numCourses)}, True
+        
+        for end, start in prerequisites:
+            edges[start].append(end)
+        
+        def dfs(node):
+            nonlocal is_possible, i
+            color[node] = 1
+            for v in edges[node]:
+                if color[v] == 0: dfs(v)
+                if color[v] == 1: is_possible = False
+            color[node] = 2; ans[i] = node; i -= 1
+            
+        i = numCourses - 1
+        for v in range(numCourses):
+            if color[v] == 0: dfs(v)
+            
+        return ans if is_possible else []
---
+++ graph/Reconstruct_Itinerary.py
@@ -0,0 +1,49 @@
+'''
+link:https://leetcode.com/problems/reconstruct-itinerary/
+
+Given a list of airline tickets represented by pairs of departure and arrival airports [from, to], reconstruct the itinerary in order. All of the tickets belong to a man who departs from JFK. Thus, the itinerary must begin with JFK.
+
+Note:
+
+If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string. For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+All airports are represented by three capital letters (IATA code).
+You may assume all tickets form at least one valid itinerary.
+Example 1:
+
+Input: [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+Output: ["JFK", "MUC", "LHR", "SFO", "SJC"]
+Example 2:
+
+Input: [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
+Explanation: Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"].
+             But it is larger in lexical order.
+
+'''
+
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        
+        count, edges, N = defaultdict(int), defaultdict(list), len(tickets)
+        for start, end in tickets:
+            count[(start, end)] += 1
+            edges[start].append(end)
+        
+        for start, _ in tickets:
+            edges[start] = sorted(edges[start])
+        
+        def dfs(node, path=[]):
+            path.append(node)
+            if len(path) == N + 1: return path
+            for v in edges[node]:
+                if not count[node, v]: continue
+                count[node, v] -= 1
+                res = dfs(v, path)
+                if len(res) == N + 1: return res
+                count[node, v] += 1; path.pop()
+            return path
+        return dfs('JFK')
