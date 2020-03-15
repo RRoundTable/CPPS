420400d8d8e703dde9905eb3944a63f8063e4d7e
wontak <ryu071511@gmail.com>
2020-03-15 13:36:28 +0900

    add

---
+++ DP/Delete_and_Earn.py
@@ -0,0 +1,45 @@
+'''
+link:https://leetcode.com/problems/delete-and-earn/
+
+Given an array nums of integers, you can perform operations on the array.
+
+In each operation, you pick any nums[i] and delete it to earn nums[i] points. After, you must delete every element equal to nums[i] - 1 or nums[i] + 1.
+
+You start with 0 points. Return the maximum number of points you can earn by applying such operations.
+
+Example 1:
+
+Input: nums = [3, 4, 2]
+Output: 6
+Explanation: 
+Delete 4 to earn 4 points, consequently 3 is also deleted.
+Then, delete 2 to earn 2 points. 6 total points are earned.
+ 
+
+Example 2:
+
+Input: nums = [2, 2, 3, 3, 3, 4]
+Output: 9
+Explanation: 
+Delete 3 to earn 3 points, deleting both 2's and the 4.
+Then, delete 3 again to earn 3 points, and 3 again to earn 3 points.
+9 total points are earned.
+ 
+
+Note:
+
+The length of nums is at most 20000.
+Each element nums[i] is an integer in the range [1, 10000].
+ 
+'''
+from collections import Counter
+
+
+class Solution:
+    '''O(10000)/O(N)'''
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        if not nums: return 0
+        dp = Counter(nums)
+        for k in range(1, max(nums)+1):
+            dp[k] = max(dp.get(k-2, 0) + dp[k] * k, dp.get(k-1, 0))
+        return dp[k]
